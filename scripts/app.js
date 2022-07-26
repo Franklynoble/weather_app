7074f7074a0e04a9b50730b21ddf34eba74e1567
Franklynoble <Franklynoblez@gmail.com>
2022-07-26 18:54:48 +0100

    add icon for weather

--- scripts/app.js
+++ scripts/app.js
@@ -38,13 +38,16 @@ const updateUI =(data) => {
 
    icon.setAttribute('src', iconSrc)
 
- let timeSrc = null;
-    if(weather.IsDayTime) {
- timeSrc = 'img/day.svg';
-    }else {
-   timeSrc = 'img/night.svg';
-    }
-
+ //let timeSrc = null;
+    //using  the tenary Operator  instead of if else
+     const timeSrc = weather.IsDayTime ? 'img/day.svg': 'img/night.svg';
+// 
+//     if(weather.IsDayTime) {
+//  timeSrc = 'img/day.svg';
+//     }else {
+//    timeSrc = 'img/night.svg';
+//     }
+// 
     time.setAttribute('src', timeSrc);
 
 
